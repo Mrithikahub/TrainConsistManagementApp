9492204cba73aa278925b8e6465643c73e819d93
Mrithikahub <mrithikakumar1112@gmail.com>
2026-03-26 10:30:39 +0530

    UC10: Calculate total seating capacity using reduce

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -28,17 +28,12 @@ public class TrainConsistApp {
         bogies.add(new Bogie("Sleeper", 72));
         bogies.add(new Bogie("AC Chair", 60));
         bogies.add(new Bogie("First Class", 48));
-        bogies.add(new Bogie("Sleeper", 70)); // duplicate type
 
-        // UC9: Grouping bogies by type
-        Map<String, List<Bogie>> groupedBogies =
-                bogies.stream()
-                        .collect(Collectors.groupingBy(b -> b.name));
+        // UC10: Total capacity using reduce
+        int totalSeats = bogies.stream()
+                .map(b -> b.capacity)
+                .reduce(0, Integer::sum);
 
-        System.out.println("\nGrouped Bogies:\n");
-
-        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-        }
+        System.out.println("\nTotal Seating Capacity: " + totalSeats);
     }
 }
