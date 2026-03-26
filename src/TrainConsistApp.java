cba8eafec88cbc127113d61ac39653a8e1d5ae5e
Mrithikahub <mrithikakumar1112@gmail.com>
2026-03-26 10:28:38 +0530

    UC9: Group bogies using Collectors.groupingBy

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -28,23 +28,17 @@ public class TrainConsistApp {
         bogies.add(new Bogie("Sleeper", 72));
         bogies.add(new Bogie("AC Chair", 60));
         bogies.add(new Bogie("First Class", 48));
+        bogies.add(new Bogie("Sleeper", 70)); // duplicate type
 
-        // UC7: Sorting
-        bogies.sort(Comparator.comparingInt(b -> b.capacity));
+        // UC9: Grouping bogies by type
+        Map<String, List<Bogie>> groupedBogies =
+                bogies.stream()
+                        .collect(Collectors.groupingBy(b -> b.name));
 
-        System.out.println("\nBogies sorted by capacity:\n");
-        for (Bogie b : bogies) {
-            System.out.println(b);
-        }
+        System.out.println("\nGrouped Bogies:\n");
 
-        // UC8: Filtering using Stream
-        List<Bogie> filteredBogies = bogies.stream()
-                .filter(b -> b.capacity > 60)
-                .collect(Collectors.toList());
-
-        System.out.println("\nFiltered Bogies (Capacity > 60):\n");
-        for (Bogie b : filteredBogies) {
-            System.out.println(b);
+        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
         }
     }
 }
